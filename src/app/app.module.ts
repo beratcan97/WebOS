fb60110633ba2aea0bb939bee05f5778654747e7
unknown <beratcan_Acil@hotmail.se>
2018-11-03 22:40:52 +0100

    components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeScreenComponent } from './components/home-screen/home-screen.component';
+import { TopBarComponent } from './components/home-screen/top-bar/top-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeScreenComponent,
+    TopBarComponent,
   ],
   imports: [
     BrowserModule
