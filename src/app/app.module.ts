73e21d5a170a3322fb9eee16d7aa7936b4ba166e
unknown <beratcan_Acil@hotmail.se>
2018-11-04 08:21:27 +0100

    components, background, apple stuff and no select added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeScreenComponent } from './components/home-screen/home-screen.component';
 import { TopBarComponent } from './components/home-screen/top-bar/top-bar.component';
+import { DockComponent } from './components/home-screen/dock/dock.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeScreenComponent,
     TopBarComponent,
+    DockComponent,
   ],
   imports: [
     BrowserModule
