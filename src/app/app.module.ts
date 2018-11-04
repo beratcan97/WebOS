a75548901cfa5ab43512f20d24ea39ef5f4ecc25
unknown <beratcan_Acil@hotmail.se>
2018-11-04 09:13:01 +0100

    app icons testing´

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeScreenComponent } from './components/home-screen/home-screen.component';
 import { DockComponent } from './components/home-screen/dock/dock.component';
+import { AppsComponent } from './components/apps/apps.component';
+import { ClockComponent } from './components/apps/clock/clock.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeScreenComponent,
     DockComponent,
+    AppsComponent,
+    ClockComponent,
   ],
   imports: [
     BrowserModule
