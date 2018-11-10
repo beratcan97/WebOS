ac4c951698e203846a89dd9c7b6222e830ab6ce8
unknown <beratcan_Acil@hotmail.se>
2018-11-10 14:22:41 +0100

    gh pages testing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,24 +4,25 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
-/*
+
 import { HomeScreenComponent } from './components/home-screen/home-screen.component';
+/*
 import { DockComponent } from './components/home-screen/dock/dock.component';
 import { AppsLogosComponent } from './components/home-screen/apps-logos/apps-logos.component';
 import { FacebookLogoComponent } from './components/home-screen/apps-logos/facebook/facebook.component';
 import { FacebookComponent } from './components/apps/facebook/facebook.component';
 import { AppsNavbarComponent } from './components/apps/apps-navbar/apps-navbar.component';
-
+*/
 const appRoutes: Routes = [
   { path: '', component: HomeScreenComponent },
-  { path: 'apps/facebook', component: FacebookComponent }
+  //{ path: 'apps/facebook', component: FacebookComponent }
 ];
-*/
+
 @NgModule({
   declarations: [
     AppComponent,
-    /*
     HomeScreenComponent,
+    /*
     DockComponent,
     AppsLogosComponent,
     FacebookLogoComponent,
@@ -29,11 +30,11 @@ const appRoutes: Routes = [
     AppsNavbarComponent,*/
   ],
 
-  imports: [/*
-      RouterModule.forRoot(
-        appRoutes,
-        { enableTracing: true } // <-- debugging purposes only
-      ),*/
+  imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule
   ],
   providers: [],
