f83f63edfa8755e6a88b9907e948e280d40fb2c7
unknown <beratcan_Acil@hotmail.se>
2018-11-13 21:24:05 +0100

    router testing

--- src/app/components/home-screen/apps-logos/news-logo/news-logo.component.ts
+++ src/app/components/home-screen/apps-logos/news-logo/news-logo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-news-logo',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewsLogoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  navigate() {
+    this.router.navigate(['/news']);
+  }
 }
