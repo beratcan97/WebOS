b8022b82f826c0bcfaaebeb016e72ea5937e43f5
unknown <beratcan_Acil@hotmail.se>
2018-11-16 21:00:49 +0100

    testing of the method to see if the pwa is installed or not

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'WebOS';
+  isInstalled: boolean = false;
   isLoaded: boolean = false;
 
   ngOnInit() {
+
+    if (window.matchMedia('(display-mode: standalone)').matches) {
+      this.isInstalled = true;
+      // do things here  
+      // set a variable to be used when calling something  
+      // e.g. call Google Analytics to track standalone use   
+    }
+
+
     this.delay(3000).then(any => {
       this.isLoaded = true;
     });
