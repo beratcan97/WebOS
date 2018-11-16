b666b46040fa19da8a1c2b56aba5ed7848a43b82
unknown <beratcan_Acil@hotmail.se>
2018-11-16 21:32:53 +0100

    trying to fix the bug that makes the pwa give error 404

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,15 +11,6 @@ export class AppComponent {
   isLoaded: boolean = false;
 
   ngOnInit() {
-
-    if (window.matchMedia('(display-mode: standalone)').matches) {
-      this.isInstalled = true;
-      // do things here  
-      // set a variable to be used when calling something  
-      // e.g. call Google Analytics to track standalone use   
-    }
-
-
     this.delay(3000).then(any => {
       this.isLoaded = true;
     });
