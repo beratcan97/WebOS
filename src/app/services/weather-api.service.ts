6b02b874d4ae214e3b4176684ae3fc40081a3eb8
unknown <beratcan_Acil@hotmail.se>
2018-11-22 18:11:55 +0100

    changed weather api to htpps

--- src/app/services/weather-api.service.ts
+++ src/app/services/weather-api.service.ts
@@ -13,6 +13,6 @@ export class WeatherAPIService {
   constructor(private http: HttpClient) { }
 
   getWeather(): Observable<any> {
-    return this.http.get('http://api.openweathermap.org/data/2.5/forecast?q=Stockholm,se&units=metric&APPID=902e20f143484cc21bdab9d80c5ef9cd');
+    return this.http.get('https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,se&units=metric&APPID=902e20f143484cc21bdab9d80c5ef9cd');
   }
 }
