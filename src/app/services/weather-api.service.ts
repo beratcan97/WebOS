5b7e022a2e55e7f9b0210a26295417f7f39bc138
unknown <beratcan_Acil@hotmail.se>
2018-11-26 20:10:13 +0100

    weather api key changed

--- src/app/services/weather-api.service.ts
+++ src/app/services/weather-api.service.ts
@@ -13,6 +13,6 @@ export class WeatherAPIService {
   constructor(private http: HttpClient) { }
 
   getWeather(): Observable<any> {
-    return this.http.get('https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,se&units=metric&APPID=902e20f143484cc21bdab9d80c5ef9cd');
+    return this.http.get('https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,se&units=metric&APPID=bc399329a40cf183523b1b5d4e8ed33b');
   }
 }
