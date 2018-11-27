06fd345856e9324889d74337309edfa76bb6ca07
unknown <beratcan_Acil@hotmail.se>
2018-11-27 18:21:06 +0100

    fixed the console error on news app

--- src/app/components/apps-launcher/apps/news/news.component.ts
+++ src/app/components/apps-launcher/apps/news/news.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NewsApiService } from '../../../../services/news-api.service';
 
 @Component({
   selector: 'app-news',
@@ -8,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 export class NewsComponent implements OnInit {
 
   lang = window.navigator.language;
+  engNewsDATA;
+  isEngNewsDATALoaded: boolean = false;
 
-  constructor() { }
+  constructor(private newsApiService: NewsApiService) { }
 
   ngOnInit() {
+    this.newsApiService.engNewsDATA$
+      .subscribe(
+        DATA => this.setVariables(DATA),
+        error => console.log(error),
+      );
+  }
+
+  setVariables(DATA) {
+    this.engNewsDATA = DATA;
+    this.isEngNewsDATALoaded = true;
   }
 }
