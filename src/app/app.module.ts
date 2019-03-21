1aac71e3d1cc80e0491a5e6cce527e804b5eaa44
beratcan97 <beratcan_acil@hotmail.se>
2019-03-21 20:07:46 +0100

    usehash

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -93,7 +93,7 @@ const appRoutes: Routes = [
   ],
 
   imports: [
-    RouterModule.forRoot(appRoutes, { useHash: false }),
+    RouterModule.forRoot(appRoutes, { useHash: true }),
     //{ enableTracing: true } // <-- debugging purposes only
     BrowserModule,
     HttpModule,
