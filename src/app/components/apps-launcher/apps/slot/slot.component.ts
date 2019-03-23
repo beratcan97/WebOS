32a57d7a15dd3cd82353eab38c6cb4b495af567b
beratcan97 <beratcan_acil@hotmail.se>
2019-03-23 21:19:09 +0100

    slots

--- src/app/components/apps-launcher/apps/slot/slot.component.ts
+++ src/app/components/apps-launcher/apps/slot/slot.component.ts
@@ -8,10 +8,52 @@ import { Component, OnInit } from '@angular/core';
 export class SlotComponent implements OnInit {
 
   lang = window.navigator.language;
+  isLoaded: boolean = false;
+
+  spinner1: number = 0;
+  spinner2: number = 0;
+  spinner3: number = 0;
+  cash: number = 10;
 
   constructor() { }
 
   ngOnInit() {
+    this.delay(3000).then(any => {
+      this.isLoaded = true;
+    });
+  }
+  async delay(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
   }
 
+  spin() {
+    if (this.cash) {
+      this.cash = this.cash - 1;
+
+      this.spinner1 = this.generateRandomNumber();
+      this.spinner2 = this.generateRandomNumber();
+      this.spinner3 = this.generateRandomNumber();
+  
+      this.checkWin();
+    } else {
+      alert("No money left");
+    }
+  }
+
+  checkWin() {
+    //All 3 same
+    if (this.spinner1 === this.spinner2 && this.spinner1 === this.spinner3) {
+      alert('jackpot');
+      this.cash = this.cash + 1001;
+    } 
+    //2 same
+    else if (this.spinner1 === this.spinner2 || this.spinner2 === this.spinner3 || this.spinner1 === this.spinner3) {
+      alert('Win');
+      this.cash = this.cash + 4;
+    }
+  }
+
+  generateRandomNumber() {
+    return Math.floor((Math.random() * 10) + 1);
+  }
 }
