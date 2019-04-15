a3ef6c11507288298c44d2a7458356a46e88519f
unknown <beratcan_Acil@hotmail.se>
2019-04-15 19:00:44 +0200

    flappy can now jump

--- src/app/components/apps-launcher/apps/flappy/flappy.component.ts
+++ src/app/components/apps-launcher/apps/flappy/flappy.component.ts
@@ -8,38 +8,28 @@ import { Component, OnInit } from '@angular/core';
 export class FlappyComponent implements OnInit {
 
   lang = window.navigator.language;
-
-  highScore: number = parseInt(localStorage.getItem('cpmGameHighScore'));
   gameRunning: boolean = true;
 
+
+  //Player
+  playerJumps = false;
+
+
   constructor() {
-    if (!localStorage.getItem('flappyGameHighScore')) {
-      this.highScore = 0;
-    }
   }
 
   ngOnInit() {
-    setInterval(() => {
-      this.updateGameScreen();
-    }, 1000)
+
+  }
+  async delay(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
   }
 
-  updateGameScreen() {
-  }
-
-  createGameArea() {
-    var canvas = document.getElementById('gameScreen');
-    if (canvas.getContext) {
-      var ctx = canvas.getContext('2d');
-
-      //Player
-      ctx.fillStyle = 'red';
-      ctx.fillRect(30, 120, 30, 30);
-
-      //Moving obstacle
-      ctx.fillStyle = 'green';
-      ctx.fillRect(300, 120, 10, 200);
-    }
+  onScreenClick() {
+    this.playerJumps = true;
+    this.delay(500).then(any => {
+      this.playerJumps = false;
+    });
   }
 
   restartGame() {
