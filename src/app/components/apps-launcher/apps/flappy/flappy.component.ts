1406af8560b64592f6ce95b4abe72af52d0fdd21
unknown <beratcan_Acil@hotmail.se>
2019-04-16 21:13:01 +0200

    score system in flappy

--- src/app/components/apps-launcher/apps/flappy/flappy.component.ts
+++ src/app/components/apps-launcher/apps/flappy/flappy.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-flappy',
   templateUrl: './flappy.component.html',
   styleUrls: ['./flappy.component.css']
 })
-export class FlappyComponent implements OnInit {
+export class FlappyComponent implements OnInit, OnDestroy {
 
   lang = window.navigator.language;
   gameRunning: boolean = true;
@@ -17,28 +17,65 @@ export class FlappyComponent implements OnInit {
   //pipe
   bottomPipe = 1;
 
+  //Score
+  highScore: number = parseInt(localStorage.getItem('flappyGameHighScore'));
+  score: number = 0;
+
+  //Game logic
+  timer;
+
   constructor() {
   }
 
   ngOnInit() {
-    setInterval(() => {
+    if (!localStorage.getItem('flappyGameHighScore')) {
+      this.highScore = 0;
+    }
+
+    this.timer = setInterval(() => {
       this.tubeUpdate();
       this.updatePlayer();
-    }, 100)
+      this.checkCrash();
+      this.updateScore();
+    }, 100);
   }
 
-  async delay(ms: number) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+  ngOnDestroy() {
+    clearInterval(this.timer);
+    this.resetGame();
   }
 
   tubeUpdate(): void {
     this.bottomPipe++;
-    this.checkCrash(this.bottomPipe);
 
     if (this.bottomPipe == 100) {
       this.bottomPipe = 1;
     } else {
       document.getElementById('bp1').style.right = (this.bottomPipe.toString() + '%');
+    }
+  }
+
+  checkCrash(): void {
+    if (this.playerBottom <= 5) {
+      //Touch floor
+      this.playerLose();
+    } else if (this.bottomPipe > 70 && this.bottomPipe < 90 && this.playerBottom < 20) {
+      //Touch bottom pipe
+      this.playerLose();
+    }
+  }
+
+  playerLose() {
+    alert('You lose');
+    this.resetGame();
+  }
+
+  updateScore(): void {
+    this.score++;
+
+    if (this.score > this.highScore) {
+      this.highScore = this.score;
+      localStorage.setItem('flappyGameHighScore', this.score.toString());
     }
   }
 
@@ -52,18 +89,14 @@ export class FlappyComponent implements OnInit {
     document.getElementById('player').style.bottom = (this.playerBottom.toString() + '%');
   }
 
-  checkCrash(bp): void {
-    if (bp > 70 && bp < 90 && this.playerBottom < 20) {
-      alert('You lose');
-      this.resetGame();
-    }
-  }
-
   resetGame() {
     //Player
     this.playerBottom = 50;
 
     //pipe
     this.bottomPipe = 1;
+
+    //Score
+    this.score = 0;
   }
 }
